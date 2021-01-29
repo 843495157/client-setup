07a5b3c1b30cb73c1be599c8dddb6a3fa1ee9bb8
ywb <ywb@boostfield.com>
2021-01-29 09:45:39 +0800

    Modify App.js

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> lucky boy and girl
+        <strong>{results.length}</strong> Test
       </p>
       <hr></hr>
       {results
